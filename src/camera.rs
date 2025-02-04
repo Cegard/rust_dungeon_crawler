004f95db0bb4abad88ffad72a466b5f57b91b85b
Eduardo Galeano <cegard689@gmail.com>
2025-02-03 22:00:51 -0500

    Displaying graphics with camera.

--- src/camera.rs
+++ src/camera.rs
@@ -17,7 +17,7 @@ impl Camera {
         }
     }
 
-    pub fn on_player_position(&mut self, player_position: Point) {
+    pub fn on_player_move(&mut self, player_position: Point) {
         self.left_x = player_position.x - DISPLAY_WIDTH / 2;
         self.right_x = player_position.x + DISPLAY_WIDTH / 2;
         self.top_y = player_position.y - DISPLAY_HEIGHT / 2;
