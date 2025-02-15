847452df3afbd0675eeda51c2a72dc0e7febfabd
Eduardo Galeano <cegard689@gmail.com>
2025-02-14 22:59:34 -0500

    Walking enemies

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -3,6 +3,7 @@ mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
+mod random_move;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
@@ -10,5 +11,7 @@ pub fn build_scheduler() -> Schedule {
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(random_move::random_move_system())
         .build()
 }
