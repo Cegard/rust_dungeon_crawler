4c498cab94741b09cdda915078346b4c6811ddc5
Eduardo Galeano <cegard689@gmail.com>
2025-02-19 22:17:09 -0500

    Displaying tooltips on monsters

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -7,6 +7,7 @@ mod map_render;
 mod movement;
 mod player_input;
 mod random_move;
+mod tooltips;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -15,6 +16,7 @@ pub fn build_input_scheduler() -> Schedule {
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
 
@@ -22,10 +24,10 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(map_render::map_render_system())
-        .add_system(entity_render::entity_render_system())
         .add_system(collisions::collisions_system())
         .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
@@ -33,14 +35,12 @@ pub fn build_player_scheduler() -> Schedule {
 
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(map_render::map_render_system())
-        .add_system(entity_render::entity_render_system())
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(random_move::random_move_system())
         .flush()
         .add_system(movement::movement_system())
         .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
