a59e69e5090e866a96c81387013cc371214cdd04
Eduardo Galeano <cegard689@gmail.com>
2025-04-11 23:36:05 -0500

    Ready to consume items

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -38,6 +38,15 @@ pub fn player_input(
                     });
                 Point::new(0, 0)
             }
+            VirtualKeyCode::Key1 => use_item(0, ecs, commands),
+            VirtualKeyCode::Key2 => use_item(1, ecs, commands),
+            VirtualKeyCode::Key3 => use_item(2, ecs, commands),
+            VirtualKeyCode::Key4 => use_item(3, ecs, commands),
+            VirtualKeyCode::Key5 => use_item(4, ecs, commands),
+            VirtualKeyCode::Key6 => use_item(5, ecs, commands),
+            VirtualKeyCode::Key7 => use_item(6, ecs, commands),
+            VirtualKeyCode::Key8 => use_item(7, ecs, commands),
+            VirtualKeyCode::Key9 => use_item(8, ecs, commands),
             _ => Point::new(0, 0),
         };
 
@@ -90,5 +99,38 @@ pub fn player_input(
         }
 
         *turn_state = TurnState::PlayerTurn;
+    }
+
+    fn use_item(n: usize, ecs: &mut SubWorld, commands: &mut CommandBuffer) -> Point {
+        let player_entity = <(Entity, &Player)>::query()
+            .iter(ecs)
+            .map(|(entity, _player)| Some(*entity))
+            .next()
+            .flatten()
+            .unwrap();
+
+        let item_entity = <(Entity, &Item, &Carried)>::query()
+            .iter(ecs)
+            .filter(|(_, _, carried)| carried.0 == player_entity)
+            .enumerate()
+            .find_map(|(item_count, (item_entity, _, _))| {
+                if item_count == n {
+                    Some(*item_entity)
+                } else {
+                    None
+                }
+            });
+
+        if let Some(item_entity) = item_entity {
+            commands.push((
+                (),
+                ActivateItem {
+                    used_by: player_entity,
+                    item: item_entity,
+                },
+            ));
+        }
+
+        Point::zero()
     }
 }
